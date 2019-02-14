7ec36cc00b8d71d8f60e1ab88ab858bd4416e379
bagasghufronalfaiz <bagasghufronalfaiz@users.noreply.github.com>
2019-02-14 20:59:22 +0700

    Update categories.php

--- admin/categories.php
+++ admin/categories.php
@@ -15,7 +15,7 @@
                             <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
                         </li>
                         <li class="active">
-                            <i class="fa fa-file"></i> Blank Page
+                            <i class="fa fa-file"></i> Categories
                         </li>
                     </ol>
                 </div>
